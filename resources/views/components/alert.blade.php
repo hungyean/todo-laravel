9edfdb00f41f5d022c5e4c0028f830c58ad69915
hungyean <hungyean@goodsane.com>
2019-01-21 16:10:26 +0800

    finish commit

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,6 +1,4 @@
 <!-- /resources/views/alert.blade.php -->
 <div class="alert alert-danger">
-    <div class="alert-title">{{ $title }}</div>
-
     {{ $slot }}
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,11 @@
 @extends("layouts.app")
 @section("content")
 <div class="container">
+<?php
+#@alert(['type' => 'danger'])
+ #       You are not allowed to access this resource!
+#@endalert
+?>
 @if(session()->has('message'))
 <div class="alert alert-success alert-dismissible fade show" role="alert">
 <strong>Done !!! </strong>{{ session()->get('message') }}
@@ -29,12 +34,25 @@
 </div>
 </form>
 <hr>
+<h4>Show loops</h4>
+@foreach ($tasks as $task)
+    @if ($loop->first)
+        This is the first iteration.
+    @endif
+
+    @if ($loop->last)
+        This is the last iteration.
+    @endif
+
+    <p>This is user {{ $task->id }}</p>
+@endforeach
+
 <ol>
 @foreach($tasks as $task)
 <li>{{ $task->task }}&nbsp;&nbsp;&nbsp;<a class="btn btn-success" href ={{url('/'.$task->id.'/complete')}}>Mark As Completed</a>&nbsp;&nbsp;&nbsp;<a class="btn btn-danger" href ={{url('/'.$task->id.'/delete')}}>Delete</a>&nbsp;&nbsp;&nbsp;<a class="btn btn-info" href ={{url('/'.$task->id.'/edit')}}>Edit</a></li><br>
 @endforeach
 </ol>
-<h4>Completedd</h4>
+<h4>Completed</h4>
 <ol>
 @foreach($completed_tasks as $c_task)
 <li>Completed &nbsp;{{ $c_task->task }}&nbsp;&nbsp;&nbsp;<a class="btn btn-warning" href ={{url('/'.$c_task->id.'/incomplete')}}>Mark As Incomplete</a>&nbsp;&nbsp;&nbsp;<a class="btn btn-danger" href ={{url('/'.$c_task->id.'/delete')}}>Delete</a>&nbsp;&nbsp;&nbsp;<a class="btn btn-info" href ={{url('/'.$c_task->id.'/edit')}}>Edit</a></li>
